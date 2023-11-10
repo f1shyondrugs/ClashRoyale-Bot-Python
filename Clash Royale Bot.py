eb8a8f92282dd480dec521ade0d0103b730c6e06
Das_F1sHy312 <86548888+f1shyondrugs@users.noreply.github.com>
2023-11-10 16:40:43 +0100

    Add files via upload

---
+++ Clash Royale Bot.py
@@ -0,0 +1,76 @@
+# Clash royale spammer
+
+import getpixelcolor
+import pyautogui
+import time
+import random
+
+def checks():
+    papa = getpixelcolor.pixel(1650, 1000)
+    if papa == ( 78, 175, 255):
+        print("Starting Round!")
+
+
+        
+        pyautogui.moveTo(1650, 1000)
+        pyautogui.leftClick()
+        time.sleep(1)
+
+
+    papa = getpixelcolor.pixel(1507, 668)
+    if papa == (255, 186,   0):
+        
+
+
+            
+        pyautogui.moveTo(1507, 668)
+        pyautogui.leftClick()
+        time.sleep(10)
+
+
+
+while 1:
+
+    checks()
+
+
+
+    pyautogui.moveTo(1522, 900)
+    pyautogui.leftClick()
+    time.sleep(0.5)
+    checks()
+    pyautogui.moveTo(random.randint(1315,1874), random.randint(550,811))
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    checks()
+    pyautogui.moveTo(1599, 900)
+    pyautogui.leftClick()
+    time.sleep(0.5)
+    checks()
+    pyautogui.moveTo(random.randint(1315,1874), random.randint(550,811))
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    checks()
+
+    pyautogui.moveTo(1663, 900)
+    pyautogui.leftClick()
+    time.sleep(0.5)
+    checks()
+    pyautogui.moveTo(random.randint(1315,1874), random.randint(550,811))
+    pyautogui.leftClick()
+    time.sleep(1)
+
+
+    checks()
+
+
+    pyautogui.moveTo(1809, 900)
+    pyautogui.leftClick()
+    time.sleep(0.5)
+    checks()
+    pyautogui.moveTo(random.randint(1315,1874), random.randint(550,811))
+    pyautogui.leftClick()
+    time.sleep(1)
+    
